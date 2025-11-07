44d1a65bfebbeaa89ab76fe9dbf26164223c7637
Gian Lucas <gianlucassmith34@gmail.com>
2025-11-07 13:39:11 -0300

    fix: prisma error

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from 'next'
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) config.plugins = [...config.plugins, new PrismaPlugin()]
+
+    return config
+  },
+}
 
 export default nextConfig
